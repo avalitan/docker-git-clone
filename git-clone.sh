c30c14f6202e1dbaca3a488e5d417f69853ad3b3
call <6537446+call@users.noreply.github.com>
2018-08-29 11:09:04 -0700

    Add chowning

--- git-clone.sh
+++ git-clone.sh
@@ -5,15 +5,18 @@ GIT_CLONE_BRANCH=${GIT_CLONE_BRANCH:-}
 GIT_CLONE_DEST=${GIT_CLONE_DEST:-}
 GIT_CLONE_REV=${GIT_CLONE_REV:-}
 
+# Ensure directory exists
+mkdir -p $GIT_CLONE_DEST
+
 # Clone the repository
-git clone --no-checkout -b ${GIT_CLONE_BRANCH} ${GIT_CLONE_REPO} ${GIT_CLONE_DEST}
-cd ${GIT_CLONE_DEST}
+git clone --no-checkout -b $GIT_CLONE_BRANCH $GIT_CLONE_REPO $GIT_CLONE_DEST
+cd $GIT_CLONE_DEST
 
 # Fetch branch
-git fetch origin ${GIT_CLONE_BRANCH}
+git fetch origin $GIT_CLONE_BRANCH
 
 # Reset to revision
-git reset --hard ${GIT_CLONE_REV}
+git reset --hard $GIT_CLONE_REV
 
-chown -R 1000:1000 ${GIT_CLONE_DEST}
+chown -R 1000:1000 $GIT_CLONE_DEST
 
