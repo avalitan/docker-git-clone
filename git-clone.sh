e0862233db22098fcdaa02d3d81767125082204f
call <6537446+call@users.noreply.github.com>
2018-08-29 13:04:02 -0700

    Need to cd

--- git-clone.sh
+++ git-clone.sh
@@ -7,13 +7,14 @@ GIT_CLONE_REV=${GIT_CLONE_REV:-}
 
 # Ensure directory exists
 mkdir -p $GIT_CLONE_DEST
+cd $GIT_CLONE_DEST
 
 # Clone the repository
 git clone --no-checkout -b $GIT_CLONE_BRANCH $GIT_CLONE_REPO $GIT_CLONE_DEST
 
 # Fetch branch
-git fetch origin $GIT_CLONE_BRANCH $GIT_CLONE_DEST
+git fetch origin $GIT_CLONE_BRANCH
 
 # Reset to revision
-git reset --hard $GIT_CLONE_REV $GIT_CLONE_DEST
+git reset --hard $GIT_CLONE_REV
 
