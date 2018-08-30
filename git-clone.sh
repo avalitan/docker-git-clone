b72a9d933c536d252a928da845c86540632f3fdf
call <6537446+call@users.noreply.github.com>
2018-08-29 19:52:33 -0700

    Shallow clone and mv

--- git-clone.sh
+++ git-clone.sh
@@ -4,16 +4,8 @@ GIT_CLONE_REPO=${GIT_CLONE_REPO:-}
 GIT_CLONE_BRANCH=${GIT_CLONE_BRANCH:-}
 GIT_CLONE_DEST=${GIT_CLONE_DEST:-}
 GIT_CLONE_REV=${GIT_CLONE_REV:-}
+TMP_DIR=$(mktemp -d)
 
-# Ensure directory exists
+git clone --depth=1 -b $GIT_CLONE_BRANCH $GIT_CLONE_REPO $TMP_DIR
 mkdir -p $GIT_CLONE_DEST
-cd $GIT_CLONE_DEST
-
-# Clone the repository
-git clone --no-checkout -b $GIT_CLONE_BRANCH $GIT_CLONE_REPO $GIT_CLONE_DEST
-
-# Fetch branch
-git fetch origin $GIT_CLONE_BRANCH
-
-# Reset to revision
-git reset --hard $GIT_CLONE_REV
+mv $TMP_DIR $GIT_CLONE_DEST
